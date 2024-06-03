ce3c52bde4dae39c5945bcfafff6d819ff7fecaa
Alexandre Silva <127327458+AlSilDev@users.noreply.github.com>
2024-06-03 19:57:45 +0100

    feat: created message field component

---
+++ frontend/src/MessageField.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Disclosure, Button } from "@headlessui/react";
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+
+export default function Chat() {
+    const [msg, setMsg] = useState('');
+
+    function sendMsg(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const target = event.target as typeof event.target & {
+            msg: { value: string };
+        };
+        const msg = target.msg.value; // 'msg' is the name of input element
+        setMsg(msg);
+    }
+
+    return (
+        <Disclosure>
+                <form onSubmit={sendMsg}>
+                    <div className="flex flex-row justify-stretch">
+                        <input type="text" name="msg" placeholder="Type a message..." className="grow p-2 bg-gray-200 dark:bg-gray-800 dark:text-gray-200"/>
+                        <Button type="submit" className="grow-0 rounded dark:bg-gray-700 py-2 px-4 text-sm text-white dark:data-[hover]:bg-gray-600 data-[active]:bg-sky-700">
+                            <PaperAirplaneIcon className="h-6 w-auto"/>
+                        </Button>
+                    </div>
+                </form>
+        </Disclosure>
+    )
+}
+    
