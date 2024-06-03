b4e064c997159b6ead19e727df55397f424f250c
Alexandre Silva <127327458+AlSilDev@users.noreply.github.com>
2024-06-03 19:57:31 +0100

    feat: created chat component

---
+++ frontend/src/Chat.tsx
@@ -0,0 +1,15 @@
+import { Disclosure } from '@headlessui/react';
+
+const msgs = [{sentBy: 'Alice', content: 'Hello!'}, {sentBy: 'Bob', content: 'Hi!'}, {sentBy: 'Alice', content: 'How are you?'}, {sentBy: 'Bob', content: 'Good!'}, {sentBy: 'Alice', content: 'Great!'}]
+
+export default function Chat() {
+return (
+    <Disclosure>
+        {msgs.map((msg) => (
+            <div key={msg.sentBy} className="px-2 py-2">
+                <p className="text-sm text-gray-600 dark:text-gray-400 font-thin">{msg.sentBy}: {msg.content}</p>
+            </div>
+        ))}
+    </Disclosure>
+)
+}
