2c8a54c2b2e73a6ae1aa3ac3ca8d8c2e36cae3fb
Edgar Bagagem <edgarbagagem@gmail.com>
2024-06-17 22:24:49 +0100

    feat: add react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./routes/login.tsx";
+import ErrorPage from "./error-page.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
